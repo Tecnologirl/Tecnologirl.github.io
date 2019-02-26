6d403bb7616d63cd8d2a9cf9ef2d0c19d10b6df2
Alondra Mejía <31838284+TechnologirlMx@users.noreply.github.com>
2019-02-26 08:22:04 -0600

    hey

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import ReactDom from 'react-dom' // renderiza en el navegador
 
 //  console.log('Hola mundo!')
 
-const app = document.getElementsById('app')
+const app = document.getElementById('app')
 
-const holaMundo = <h1>Hola mundo</h1>
+const holaMundo = <h1>Hola mundo :D</h1>
 
 //  ReactDom.render(que se va a renderizar, donde se va a renderizar);
 ReactDom.render(holaMundo, app)
---
+++ src/media/media.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Media extends React.component {
+  render () {
+    return (
+      <div>
+        <img src=" "
+          alt=" "
+          width={260}
+          height={160}
+        />
+      </div>
+    )
+  }
+}
